aadc00ef99f65f5b23786a03492692c6a7eae02e
flo7ian <florian.wegelin@dasch.swiss>
2025-09-25 10:33:45 +0200

    v2.2.0

--- fileidentification/policies.py
+++ fileidentification/policies.py
@@ -1,21 +1,21 @@
 import json
-from dataclasses import dataclass, field, asdict
+from dataclasses import asdict, dataclass, field
 from pathlib import Path
 from typing import Any
-from fileidentification.models import BasicAnalytics
+from pydantic import BaseModel, Field
+
 from fileidentification.conf.settings import JsonOutput
-from fileidentification.policies.default import default_policies
+from fileidentification.models import BasicAnalytics
 
 
-@dataclass
-class PolicyParams:
-    format_name: str = field(default_factory=str)
-    bin: str = field(default_factory=str)
-    accepted: bool = True
-    target_container: str = field(default_factory=str)
-    processing_args: str = field(default_factory=str)
-    expected: list[str] = field(default_factory=list)
-    remove_original: bool = False
+class PolicyParams(BaseModel):
+    format_name: str = Field(default_factory=str)
+    bin: str = Field(default="")
+    accepted: bool = Field(default=True)
+    target_container: str = Field(default="")
+    processing_args: str = Field(default="")
+    expected: list[str] = Field(default=[""])
+    remove_original: bool = Field(default=False)
 
 
 def generate_policies(
@@ -33,13 +33,15 @@ def generate_policies(
     # blank caveat
     if blank:
         for puid in ba.puid_unique:
-            policies[puid] = asdict(PolicyParams(format_name=fmt2ext[puid]["name"]))
+            policies[puid] = PolicyParams(format_name=fmt2ext[puid]["name"]).model_dump()
         # write out policies with name of the folder, return policies and BasicAnalytics
         with open(jsonfile, "w") as f:
             json.dump(policies, f, indent=4, ensure_ascii=False)
         return policies
 
     # default values
+    default_policies = json.loads(Path("fileidentification/policies/dasch.json").read_text())
+
     ba.blank = []
     for puid in ba.puid_unique:
         # if it is run in extend mode, add the existing policy if there is any
@@ -50,7 +52,7 @@ def generate_policies(
             pass  # don't create a blank policies -> files of this type are moved to FAILED
         # if there are no default values of this filetype
         elif puid not in default_policies:
-            policies[puid] = asdict(PolicyParams(format_name=fmt2ext[puid]["name"]))
+            policies[puid] = PolicyParams(format_name=fmt2ext[puid]["name"]).model_dump()
             ba.blank.append(puid)
         else:
             policies[puid] = default_policies[puid]
